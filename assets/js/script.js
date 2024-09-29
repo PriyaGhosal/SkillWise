495f38f92a65a69748d8fe9b2d4c3149c72aa1e0
piyushshrma <securepiyush01@gmail.com>
2024-09-29 12:43:34 +0530

    added scroll to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,3 +64,31 @@ const headerActive = function () {
 }
 
 window.addEventListener("scroll", headerActive);
+
+//Scroll to top
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function () {
+            scrollFunction();
+        };
+
+        function scrollFunction() {
+            if (
+                document.body.scrollTop > 20 ||
+                document.documentElement.scrollTop > 20
+            ) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        mybutton.addEventListener("click", backToTop);
+
+        function backToTop() {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
