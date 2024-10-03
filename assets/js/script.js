50ed487cc4926b0c096cb8ef97245c1f0e39220d
Shobhit1729Aryan <shobhitaryan2003@gmail.com>
2024-10-04 01:43:35 +0530

    I aligned data science courses divs

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+'use strict';
+
+
+
+/**
+ * add eventListener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * PRELOADER
+ */
+
+const preloader = document.querySelector("[data-preloader]");
+const circle = document.querySelector("[data-circle]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  circle.style.animation = "none";
+  document.body.classList.add("loaded");
+});
+
+
+
+/**
+ * NAVBAR TOGGLER FOR MOBILE
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+
+
+/**
+ * HEADER
+ * 
+ * add active class on header when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+const headerActive = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+window.addEventListener("scroll", headerActive);
