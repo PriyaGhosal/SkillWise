6d12c95f831b7ba97f0af20557fc23093a545610
RiyaHoro <145681087+RiyaHoro@users.noreply.github.com>
2024-10-12 17:54:42 +0530

    Add files via upload

---
+++ check.js
@@ -0,0 +1,10 @@
+
+        
+function add(){
+    var num1=parseInt(document.getElementById("n1").value);
+    var num2=parseInt(document.getElementById("n2").value);
+
+    var sum=num1+num2;
+    document.getElementById("txtResult").innerHTML=sum;
+
+}
