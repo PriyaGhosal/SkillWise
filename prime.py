de13ad897dda11000e4c678f29ba810a868a48c3
vikash <120444720+vikashmurugesh@users.noreply.github.com>
2024-10-18 17:29:47 +0530

    Update prime.py
    I would like to propose the addition of a Typing Test feature to our project using Python Flask. This feature will provide users with an interactive platform to test their typing speed and accuracy.
    solved

--- prime.py
+++ prime.py
@@ -1,16 +1,13 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
 num = 11
-# Negative numbers, 0 and 1 are not primes
-if num > 1:
-  
-    # Iterate from 2 to n // 2
-    for i in range(2, (num//2)+1):
-      
-        # If num is divisible by any number between
-        # 2 and n / 2, it is not prime
-        if (num % i) == 0:
-            print(num, "is not a prime number")
-            break
-    else:
-        print(num, "is a prime number")
+if is_prime(num):
+    print(f"{num} is a prime number")
 else:
-    print(num, "is not a prime number")
+    print(f"{num} is not a prime number")
