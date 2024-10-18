68108582ee85a44402800c7906f6278fa4b1d8aa
ARVINDH-CT06 <24bct106@kgcas.com>
2024-10-18 18:46:19 +0530

    updated for calculator app

---
+++ cal.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById('myButton');
+    button.addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+}); 
