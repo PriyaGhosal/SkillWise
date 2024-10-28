3f50d6787812f04b7a602370ee32e274fdbc7124
VaishnaviUbarhande <vaishnavi15ubarhande@gmail.com>
2024-10-28 20:22:36 +0530

    Create create prime number code ...cpp

---
+++ create prime number code ...cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num) {
+    if (num <= 1)
+        return false;
+    
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPrime(num))
+        cout << num << " is a prime number." << endl;
+    else
+        cout << num << " is
